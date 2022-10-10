c0c43be515361a21014e578b3203dd7d57ffcc08
Sergey Gribinichenko <developer.kg@gmail.com>
2022-10-10 10:47:44 +0300

    Add check_mem

---
+++ net-mgmt/check_mem/files/patch-check_mem.pl
@@ -0,0 +1,9 @@
+diff -ur nagios-plugins-9d20150-orig/check_mem/check_mem.pl nagios-plugins-9d20150/check_mem/check_mem.pl
+--- check_mem/check_mem.pl	2022-09-26 16:54:06.000000000 +0300
++++ check_mem/check_mem.pl	2022-10-10 10:14:06.325271000 +0300
+@@ -1,4 +1,4 @@
+-#!/usr/bin/perl -w
++#!/usr/local/bin/perl -w
+ 
+ # Heavily based on the script from:
+ # check_mem.pl Copyright (C) 2000 Dan Larsson <dl@tyfon.net>
