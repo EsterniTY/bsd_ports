f54f78eb9baf4a42602b379d777051838ad714cb
Sergey Gribinichenko <developer.kg@gmail.com>
2023-08-31 14:39:28 +0300

    Add net-mgmt/icinga_check_mysql_stats

---
+++ net-mgmt/icinga_check_mysql_stats/files/patch-check_mysql_stats.py
@@ -0,0 +1,8 @@
+--- check_mysql_stats.py.orig	2023-08-31 12:10:48.771458000 +0300
++++ check_mysql_stats.py	2023-08-31 12:10:53.327353000 +0300
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python3.9 -u
++#!/usr/local/bin/python3.9 -u
+ # -*- coding: utf-8 -*-
+ import argparse
+ import sys
