68c7c96d4abc903dd944f7b94f4cfb7f2c38b3ca
Sergey Gribinichenko <developer.kg@gmail.com>
2023-08-31 12:08:57 +0300

    Add FreeBSD patch

---
+++ net-mgmt/icinga_check_mysql_schema_stats/files/patch-check_mysql_schema_stats.py
@@ -0,0 +1,8 @@
+--- check_mysql_schema_stats.py.orig	2023-08-30 11:01:48.771458000 +0300
++++ check_mysql_schema_stats.py	2023-08-30 11:01:53.327353000 +0300
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python3.9 -u
++#!/usr/local/bin/python3.9 -u
+ # -*- coding: utf-8 -*-
+ import argparse
+ import sys
